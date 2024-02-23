051ee81d2baba461851a709986c117d3b16789f2
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-02-22 22:11:59 -0500

    Add files via upload

--- new_utils.py
+++ new_utils.py
@@ -16,3 +16,22 @@ def scale_data(X):
     scaled_X = (X - X.min()) / (X.max() - X.min())
     
     return scaled_X
+
+
+def compute_acc_confusion_matx(confusion_matrix):
+    """
+    Calculate accuracy from a confusion matrix.
+    """
+
+    TruePositive = confusion_matrix[1, 1]  # True Positives
+    TrueNegative = confusion_matrix[0, 0]  # True Negatives
+    total_samples = confusion_matrix.sum() 
+
+    accuracy = (TruePositive+ TrueNegative) / total_samples
+    
+    return accuracy
+
+
+
+
+
